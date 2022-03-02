af53f9ff53d8cfc8273381196404f0622ab83399
Roman <roman.semko@gmail.com>
2022-03-02 09:33:17 +0100

    Removes debris

--- contracts/TestCoin.sol
+++ contracts/TestCoin.sol
@@ -4,8 +4,6 @@ pragma solidity 0.8.4;
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 contract TestCoin2 is ERC20Upgradeable {
-    address public constant BRIDGE_ADDRESS = address(0xdead);
-    bytes32 public constant BRIDGE_ROLE = keccak256("BRIDGE");
     mapping(uint256 => bytes) public data;
 
     function initialize() public initializer {
